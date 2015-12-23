971e1b21e3c71d11f10cca0134f4e66c46245b9a
tmorozov <tmorozov@timurs-mbp.us.int.genesyslab.com>
2015-12-23 21:23:39 +0300

    sbt_init.sh: updated and plugins.sbt added

--- sbt_init.sh
+++ sbt_init.sh
@@ -9,6 +9,31 @@ version := "1.0"
 
 scalaVersion := "2.10.0"
 
-libraryDependencies ++=Seq(
-    specs2 % Test
-)' > build.sbt
+scalacOptions    := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+scalacOptions in Test   ++= Seq("-Yrangepos")
+
+ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
+
+libraryDependencies ++= {
+    val akkaVer = "2.3.9"
+    val sprayVer    = "1.3.3"
+    Seq(
+        "io.spray"          %% "spray-can"      % sprayVer,
+        "io.spray"          %% "spray-routing"  % sprayVer,
+        "io.spray"          %% "spray-testkit"  % sprayVer % "test",
+        "io.spray"          %% "spray-json"     % "1.3.2",
+        "com.typesafe.akka" %% "akka-actor"     % akkaVer,
+        "com.typesafe.akka" %% "akka-testkit"   % akkaVer % "test",
+        "org.specs2"        %% "specs2-core"    % "2.3.11" % "test"
+    )
+}
+
+Revolver.settings' > build.sbt
+
+#create plugins.sbt
+echo '
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0")
+' > project/plugins.sbt
