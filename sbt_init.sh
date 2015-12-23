97d8d324e22644d3c2b82bed43230270ba1dd784
tmorozov <tmorozov@timurs-mbp.us.int.genesyslab.com>
2015-12-23 21:02:42 +0300

    sbt_init.sh added

---
+++ sbt_init.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+mkdir -p src/{main,test}/{java,resources,scala}
+mkdir lib project target
+
+#create initial build.sbt file
+echo 'name := "My Project"
+
+version := "1.0"
+
+sclaVersion := "2.10.0"
+
+libraryDependencies ++=Seq(
+    specs2 % Test
+)' > build.sbt
